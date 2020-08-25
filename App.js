d786b5503431933d8ce7a8f960ffbf762a6e374d
Sohrat <sohrat6128@gmail.com>
2020-08-25 15:33:45 +0300

    navigation created

--- App.js
+++ App.js
@@ -1,15 +1,67 @@
 import React, { } from 'react';
 import { View, Text, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import IconsM from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import Login from './src/views/login';
+import Chats from './src/views/chats';
+import Chat from './src/views/chat';
+import Users from './src/views/users';
 
 
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
+const ChatNavigation = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        options={{
+          headerShown: false
+        }}
+        name="Chats" component={Chats} />
+      <Stack.Screen name="Chat" component={Chat} />
+    </Stack.Navigator>
+  )
+}
 
 
 const App = () => {
   return (
-    <View>
+    <>
       <StatusBar backgroundColor='#1ec897' />
-      <Text>App</Text>
-    </View>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color }) => {
+              let iconName;
+
+              if (route.name === 'ChatNavigation') {
+                iconName = focused ? 'chat-processing' : 'chat-processing';
+              } else if (route.name === 'Users') {
+                iconName = focused ? 'account-circle' : 'account-circle';
+              }
+
+              return <IconsM name={iconName} size={27} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: '#1ec897',
+            inactiveTintColor: 'grey',
+            labelStyle: {
+              fontSize: 13,
+              paddingBottom: 3
+            },
+          }}
+        >
+          <Tab.Screen name="ChatNavigation" component={ChatNavigation} />
+          <Tab.Screen name="Users" component={Users} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   )
 };
 
--- src/views/chats.js
+++ src/views/chats.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
 
-const Chats = () => {
+const Chats = ({ navigation }) => {
     return (
         <View>
             <Text>Chats Page</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Chat')}><Text>go Chat page</Text></TouchableOpacity>
         </View>
     )
 };
